1136a74c25b9f7f20ec4f00bf4cd881e15ce0b0f
KW LEE <realionic@gmail.com>
2018-06-27 16:13:05 +0900

    Create kw.js

---
+++ hackerRank/Kangaroo/kw.js
@@ -0,0 +1,43 @@
+//https://www.hackerrank.com/challenges/kangaroo
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function kangaroo(x1, v1, x2, v2) {
+
+    if ( v1 === v2 ) return ( x1 === x2 )? 'YES' : 'NO'
+    if ( v2 > v1 ) return 'NO';
+
+    const z = ( x2 - x1 ) / ( v1 - v2 );
+    return Number.isInteger( z )? 'YES' : 'NO';
+    
+}
+
+function main() {
+    var x1_temp = readLine().split(' ');
+    var x1 = parseInt(x1_temp[0]);
+    var v1 = parseInt(x1_temp[1]);
+    var x2 = parseInt(x1_temp[2]);
+    var v2 = parseInt(x1_temp[3]);
+    var result = kangaroo(x1, v1, x2, v2);
+    process.stdout.write("" + result + "\n");
+}
