02fc25a8c191daee9e2070874f9bb5ed86aac495
KW LEE <realionic@gmail.com>
2018-06-27 15:38:09 +0900

    Create kw.js

---
+++ hackerRank/Matching Specific Characters/kw.js
@@ -0,0 +1,4 @@
+//https://www.hackerrank.com/challenges/matching-specific-characters
+
+var Regex_Pattern = /^[123][120][xs0][30Aa][xsu][\.\,]$/;
+//Do not delete '/'. Replace __________ with your regex. 
