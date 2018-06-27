2dd9edd965844698b39ecd484dc2fa79789e5c48
KW LEE <realionic@gmail.com>
2018-06-27 15:48:05 +0900

    Create kw.js

---
+++ hackerRank/Matching -x- y- Repetitions/kw.js
@@ -0,0 +1,4 @@
+//https://www.hackerrank.com/challenges/matching-x-y-repetitions
+
+var Regex_Pattern = /^[\d]{1,2}[a-zA-Z]{3,}[\.]{0,3}$/;
+//Do not delete '/'. Replace __________ with your regex. 
