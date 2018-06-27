6706db82424f408720eb64154e7fdff43bf57abb
KW LEE <realionic@gmail.com>
2018-06-27 15:52:26 +0900

    Create kw.js

---
+++ hackerRank/Backreferences To Failed Groups/kw.js
@@ -0,0 +1,4 @@
+//https://www.hackerrank.com/challenges/backreferences-to-failed-groups
+
+var Regex_Pattern = /^\d\d(-?)\d\d\1\d\d\1\d\d$/;
+//Do not delete '/'. Replace __________ with your regex. 
