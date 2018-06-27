bd7f9d5baa5cc34cb49d0e6870662e7027fe2ab2
KW LEE <realionic@gmail.com>
2018-06-27 15:51:18 +0900

    Create kw.js

---
+++ hackerRank/Alternative Matching/kw.js
@@ -0,0 +1,4 @@
+//https://www.hackerrank.com/challenges/alternative-matching
+
+var Regex_Pattern = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[a-zA-Z]+$/;
+//Do not delete '/'. Replace __________ with your regex. 
