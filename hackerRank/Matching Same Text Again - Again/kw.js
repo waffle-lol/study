db66c90048ec86443ad80110ab47f899aeaca2b5
KW LEE <realionic@gmail.com>
2018-06-27 15:51:52 +0900

    Create kw.js

---
+++ hackerRank/Matching Same Text Again - Again/kw.js
@@ -0,0 +1,4 @@
+//https://www.hackerrank.com/challenges/matching-same-text-again-again
+
+var Regex_Pattern = /^([a-z])(\w)(\s)([\W])(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10/;
+//Do not delete '/'. Replace __________ with your regex. 
