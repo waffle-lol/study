3dcc4e1543b12a8f42233c21d4ab61edb05081bc
KW LEE <realionic@gmail.com>
2018-06-27 22:33:41 +0900

    Create kw.js

---
+++ hackerRank/Recursion- Fibonacci Numbers/kw.js
@@ -0,0 +1,23 @@
+//https://www.hackerrank.com/challenges/ctci-fibonacci-numbers
+
+function processData(input) {
+    var n = parseInt(input);
+    console.log(fibonacci(n));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
+
+function fibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 2) + fibonacci(n - 1);
+}
