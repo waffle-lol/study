422bd2df12cdd6434068661b29aa8e4199f8b953
KW LEE <realionic@gmail.com>
2018-06-27 15:09:26 +0900

    Create kw.js

---
+++ hackerRank/Breaking the Records/kw.js
@@ -0,0 +1,60 @@
+//https://www.hackerrank.com/challenges/breaking-best-and-worst-records/submissions/code/76143133
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+  let max = scores[0];
+  let min = scores[0];
+  let maxRenewed = 0;
+  let minRenewed = 0;
+  for (let i = 1; i < scores.length; i++ ) {
+    const currentScore = scores[i];
+    if (currentScore > max) {
+      max = currentScore;
+      maxRenewed += 1;
+    } else if (currentScore < min) {
+      min = currentScore;
+      minRenewed += 1;
+    }
+  }
+  return [maxRenewed, minRenewed];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
